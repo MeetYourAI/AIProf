8ba5efbe64f272a7d1ba533558f73150577a4b0a
ibugithub <outsideworkibrahim@gmail.com>
2023-09-12 11:28:50 +0600

    Add mic next to ask btton

--- js/askMe.js
+++ js/askMe.js
@@ -1,8 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const questionInput = document.getElementById("question");
-    const askButton = document.getElementById("ask");
-    const responseDiv = document.getElementById("response");
 
+//........ LLM ChatGpt functionality...........
+const questionInput = document.getElementById("question");
+const askButton = document.getElementById("ask");
+const responseDiv = document.getElementById("response");
+
+document.addEventListener("DOMContentLoaded", () => {
     const handleAsk = async () => {
         const question = questionInput.value;
         questionInput.innerHTML = " "
@@ -72,3 +74,92 @@ document.addEventListener("DOMContentLoaded", () => {
 
     askButton.onclick = handleAsk
 });  
+
+
+// ...........mic Related js (Speech Recognition)............
+const mic = document.querySelector('#mic')
+const mutedMic = document.querySelector('#muteMic')
+let socket;
+let mediaRecorder;
+let lastUserActivityTime;
+let activityTimer;
+
+const startUsingMicrophone = () => {
+  openMic();
+  lastUserActivityTime = Date.now()
+  navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+    if (!MediaRecorder.isTypeSupported('audio/webm'))
+    {
+      muteMic();
+      return alert('Browser not supported')
+    }
+    console.log('In  navigator');
+    mediaRecorder = new MediaRecorder(stream, {
+      mimeType: 'audio/webm',
+    })
+    socket = new WebSocket('wss://api.deepgram.com/v1/listen', [
+      'token',
+      'd39b4e9d55390e5e8065a9b69966ff361a3db22d',
+    ])
+    socket.onopen = () => {
+      mediaRecorder.addEventListener('dataavailable', async (event) => {
+        if (event.data.size > 0 && socket.readyState == 1) {
+          socket.send(event.data)
+        }
+      })
+      mediaRecorder.start(1000)
+    }
+
+    socket.onmessage = (message) => {
+      const received = JSON.parse(message.data)
+      const transcript = received.channel.alternatives[0].transcript
+      if (transcript && received.is_final) {
+        lastUserActivityTime = Date.now() 
+        questionInput.value +=
+          transcript + ' '
+      }
+    }
+
+    socket.onclose = () => {
+      console.log({ event: 'onclose' })
+    }
+
+    socket.onerror = (error) => {
+      questionInput.value = 'Somethign went worng'
+      muteMic()
+      console.log({ event: 'onerror', error })
+    }
+
+    startActivityTimer()
+  })
+}
+
+const openMic = () => {
+  mic.style.display = 'inline-block';
+  mutedMic.style.display = 'none'
+} 
+
+const muteMic = () => {
+  mic.style.display = 'none';
+  mutedMic.style.display = 'inline-block'
+}
+
+const closeSocket = () => {
+  muteMic();
+  socket.close();
+  mediaRecorder.stop();
+  clearInterval(activityTimer);
+};
+
+const startActivityTimer = () => {
+  activityTimer = setInterval(() => {
+    const currentTime = Date.now();
+    const inactivityTime = currentTime - lastUserActivityTime;
+    if (inactivityTime > 10000) {
+      closeSocket();
+    }
+  }, 1000);
+};
+
+mic.addEventListener('click', closeSocket)
+mutedMic.addEventListener('click', startUsingMicrophone)
