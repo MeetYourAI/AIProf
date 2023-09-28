e3208117923d9d0fdf1726c22073bcc0312ddf27
TBalo <tolu.balogun@maxfront.com>
2023-09-28 23:20:20 +0100

    ToggleVideo

--- js/Video.js
+++ js/Video.js
@@ -1,11 +1,24 @@
-    const showVideoButton = document.getElementById('showVideoButton');
- const myVideo = document.getElementById('myVideo');
- myVideo.style.display = 'none';
+    function toggleVideo(videoId) {
+        const video = document.getElementById(videoId);
+        if (video.style.display === 'none' || video.style.display === '') {
+            video.style.display = 'block'; 
+        } else {
+            video.style.display = 'none'; 
+        }
+    }
 
- showVideoButton.addEventListener('click', function() {
-     if (myVideo.style.display === 'none') {
-         myVideo.style.display = 'block';
-     } else {
-         myVideo.style.display = 'none';
-     }
-    })
+    document.getElementById('toggleButton1').addEventListener('click', () => {
+        toggleVideo('video1');
+    });
+
+    document.getElementById('toggleButton2').addEventListener('click', () => {
+        toggleVideo('video2');
+    });
+
+    document.getElementById('toggleButton3').addEventListener('click', () => {
+        toggleVideo('video3');
+    });
+
+    document.getElementById('toggleButton4').addEventListener('click', () => {
+        toggleVideo('video4');
+    });
