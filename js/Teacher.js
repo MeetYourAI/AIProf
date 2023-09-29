321243bdb5653d91cfc25d23cc57f1cd18f7e4a8
TBalo <tolu.balogun@maxfront.com>
2023-09-29 19:23:14 +0100

    ImageGenTweak

---
+++ js/Teacher.js
@@ -0,0 +1,19 @@
+function toggleInput() {
+    const inputContainer = document.getElementById("input-container");
+    if (inputContainer.style.display === "none" || inputContainer.style.display === "") {
+        inputContainer.style.display = "block";
+    } else {
+        inputContainer.style.display = "none";
+    }
+}
+
+function changeTeacher() {
+    const teacherName = document.getElementById("inputText").value.trim();
+    if (teacherName !== "") {
+        // Replace the placeholder avatar with the selected teacher's avatar (API call).
+        // For demonstration purposes, we're just displaying a message here.
+        alert(`Teacher changed to: ${teacherName}`);
+    } else {
+        alert("Please enter a teacher's name.");
+    }
+}
