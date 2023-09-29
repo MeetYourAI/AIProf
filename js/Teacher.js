e7ddd2e0c4f60cdfc40ab9c11ea3a2a02e2539d8
TBalo <tolu.balogun@maxfront.com>
2023-09-29 20:18:52 +0100

    hideButton_replaceImage_

--- js/Teacher.js
+++ js/Teacher.js
@@ -2,18 +2,20 @@ function toggleInput() {
     const inputContainer = document.getElementById("input-container");
     if (inputContainer.style.display === "none" || inputContainer.style.display === "") {
         inputContainer.style.display = "block";
+        chooseTeacherBtn.style.display = "none"; // Hide the button
+
     } else {
         inputContainer.style.display = "none";
     }
 }
 
-function changeTeacher() {
-    const teacherName = document.getElementById("inputText").value.trim();
-    if (teacherName !== "") {
-        // Replace the placeholder avatar with the selected teacher's avatar (API call).
-        // For demonstration purposes, we're just displaying a message here.
-        alert(`Teacher changed to: ${teacherName}`);
-    } else {
-        alert("Please enter a teacher's name.");
-    }
-}
+// function changeTeacher() {
+//     const teacherName = document.getElementById("inputText").value.trim();
+//     if (teacherName !== "") {
+//         // Replace the placeholder avatar with the selected teacher's avatar (API call).
+//         // For demonstration purposes, we're just displaying a message here.
+//         alert(`Teacher changed to: ${teacherName}`);
+//     } else {
+//         alert("Please enter a teacher's name.");
+//     }
+// }
